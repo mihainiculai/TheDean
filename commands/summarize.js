5a66d4921d7a1b322884dd786976a5c1d6357e9a
Mihai Niculai <nmbroomy@gmail.com>
2023-09-22 11:06:33 +0300

    Add summarize coomand

---
+++ commands/summarize.js
@@ -0,0 +1,66 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { OpenAI } = require('openai');
+const { openAIApiKey } = require('../config.json');
+
+if (!openAIApiKey) {
+    console.error('🚫 Missing OpenAI API key. Please set it up in the config file.');
+    process.exit(1);
+}
+
+const openai = new OpenAI({
+    apiKey: openAIApiKey,
+});
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('summarize')
+        .setDescription('Summarize the last messages in this channel.')
+        .addIntegerOption(option =>
+            option.setName('hours')
+                .setDescription('How many hours back to look for messages.')
+                .setRequired(true)
+                .addChoices(
+                    { name: '30 minutes', value: 0.5 },
+                    { name: '1 hour', value: 1 },
+                    { name: '3 hours', value: 3 },
+                    { name: '6 hours', value: 6 },
+                    { name: '12 hours', value: 12 },
+                    { name: '24 hours', value: 24 },
+                )),
+
+    async execute(interaction) {
+        try {
+            await interaction.deferReply({ ephemeral: true });
+
+            const hours = interaction.options.getInteger('hours');
+            const channelMessages = await interaction.channel.messages.fetch({ limit: 100 });
+            const messages = channelMessages.filter(message => message.createdAt > Date.now() - (hours * 60 * 60 * 1000));
+
+            const conversation = messages.map(message => ({ role: 'user', content: `[${message.createdAt}] ${message.author.username}: ${message.content}` }));
+
+            const result = await openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                  ...conversation,
+                  { role: 'user', content: 'This is the conversation.' },
+                  { role: 'system', content: 'Summarize this conversation:' },
+                ],
+                max_tokens: 256,
+              });
+
+            const responseEmbed = new EmbedBuilder()
+                .setColor('#f1ac50')
+                .setTitle(`Summary of the last ${hours} hours`)
+                .addFields({ name: '\u200B', value: result.choices[0].message.content, inline: false })
+                .setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL() })
+                .setTimestamp();
+
+            await interaction.editReply({ embeds: [responseEmbed] });
+
+        } catch (error) {
+            console.error("🚫 Error at /summarize");
+            console.error(error);
+            await interaction.editReply({ content: `🚫 Oops! Something went wrong. Please try again later.`, ephemeral: true });
+        }
+    }
+}
