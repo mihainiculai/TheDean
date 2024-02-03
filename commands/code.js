c793a32e38e7cb7d5fa892e9fa3c670b647df264
Mihai Niculai <nmbroomy@gmail.com>
2024-02-03 15:54:41 +0200

    Add /code command

--- commands/code.js
+++ commands/code.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const { OpenAI } = require('openai');
 const { openAIApiKey } = require('../config.json');
 const logger = require('../logger');
@@ -14,11 +14,11 @@ const openai = new OpenAI({
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('ask')
-        .setDescription('Ask a question to The Dean.')
+        .setName('code')
+        .setDescription('Write code with The Dean.')
         .addStringOption(option =>
             option.setName('question')
-                .setDescription('What are you wondering?')
+                .setDescription('What code are you writing?')
                 .setRequired(true))
         .setDMPermission(false),
 
@@ -28,37 +28,36 @@ module.exports = {
 
             const question = interaction.options.getString('question');
             const conversation = [
-                { role: 'system', content: 'You are a friendly Discord bot named The Dean. Your are the bot of "CSIE++" discord server, the official server of CSIE (Faculty of Cybernetics, Statistics and Informatics from Bucharest). You must separate the message into paragraphs. A paragraph must have a maximum of 800 characters and separate them with "\n\n".' },
+                { role: 'system', content: 'You are a friendly Discord bot named The Dean. You must write messages formatted for Discord. If the message is too long, you must separate the message into multiple messages. Each message must have a maximum of 2000 characters. Separate the messages with "\n\n".' },
                 { role: 'user', content: question },
             ];
 
             const result = await openai.chat.completions.create({
                 model: 'gpt-4-0125-preview',
                 messages: conversation,
-                max_tokens: 256,
+                max_tokens: 512,
             });
 
             const responseContent = result.choices[0].message.content;
             const paragraphs = responseContent.split('\n\n');
 
-            const responseEmbed = new EmbedBuilder()
-                .setColor('#f1ac50')
-                .setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL() })
-                .addFields({ name: 'Question', value: question, inline: false })
+            let currentMessage = '';
+            for (const paragraph of paragraphs) {
+                if ((currentMessage + paragraph).length > 2000) {
+                    await interaction.followUp(currentMessage);
+                    currentMessage = paragraph;
+                } else {
+                    currentMessage += `\n\n${paragraph}`;
+                }
+            }
 
-            paragraphs.forEach((paragraph, index) => {
-                responseEmbed.addFields({ name: `${index === 0 ? "Answer" : "\u200B"}`, value: paragraph, inline: false });
-            });
-
-            responseEmbed
-                .setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL() })
-                .setTimestamp();
-
-            await interaction.editReply({ embeds: [responseEmbed] });
+            if (currentMessage) {
+                await interaction.followUp(currentMessage);
+            }
 
         } catch (error) {
             logger.error("🚫 Error at /ask", error);
             await interaction.editReply({ content: `🚫 Oops! Something went wrong. Please try again later.`, ephemeral: true });
         }
     }
-}
+};
