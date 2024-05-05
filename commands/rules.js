6e83d9bb533b071aaca241092703060eb20a930b
Mihai Niculai <nmbroomy@gmail.com>
2024-05-05 21:09:05 +0300

    Refactor /rules

--- commands/rules.js
+++ commands/rules.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const rules = require('../configs/rules');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,44 +10,13 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply({ ephemeral: true });
 
-        let rules = [
-            "📖 Regulament",
-            "📚 Respectați regulile de bază ale Discordului",
-            "Nu promovați violența, ura, discriminarea, conținutul ilegal sau necorespunzător excesiv.",
-            "",
-            "🤝 Fii politicos și respectuos",
-            "Comunică civilizat și respectuos, evitând limbajul ofensator, atacurile personale sau provocările excesive.",
-            "",
-            "🚫 Nu spamați",
-            "Evitați mesaje repetitive, publicitate excesivă, majuscule exagerate sau link-uri sau imagini irelevante în exces.",
-            "",
-            "🔒 Respectați confidențialitatea",
-            "Nu divulgați informații personale despre alți membri fără consimțământul lor explicit, cum ar fi nume reale, adrese, numere de telefon sau informații financiare.",
-            "",
-            "🚷 Nu faceți discriminări sau hărțuire",
-            "Nu tolerați discriminarea, hărțuirea, intimidarea sau comportament inacceptabil bazat pe rasă, sex, religie, orientare sexuală, vârstă, dizabilități sau alte caracteristici personale.",
-            "",
-            "📌 Respectați canalele și temele serverului",
-            "Postați în canalele potrivite și respectați temele specifice ale acestora. Evitați off-topicurile sau postarea repetitivă a aceluiași conținut în canale diferite.",
-            "",
-            "😠 Nu faceți trolling, flame sau provocări",
-            "Încurajați o comunicare pozitivă și constructivă în comunitate, evitând comportamentul negativ.",
-            "",
-            "🌙 Fără @everyone la ore târzii",
-            "Nu scrieți mesaje cu @everyone la ore târzii (de preferat sub ora 22:00).",
-            "",
-            "🚫 Reclame interzise",
-            "Reclamele la alte servere de discord/grupuri NU SUNT PERMISE.",
-            "ACESTA ESTE GRUPUL DE DISCORD OFICIAL ***CSIE++***.",
-            "",
-            "📩 Sugestii și recomandări",
-            "Dacă doriți să faceți o sugestie sau o recomandare, puteți contacta cu încredere un @Moderator sau un @Admin."
-        ];
+        let message = "# 📖 Regulament";
 
-        let message = rules.join('\n');
+        for (let i = 0; i < rules.length; i++) {
+            message += `\n### ${rules[i].title}\n${rules[i].description}`;
+        }
 
         await interaction.editReply({ content: 'Rules has been sent successfully!', ephemeral: true });
-
         await interaction.channel.send({ content: message });
     },
 };
---
+++ configs/rules.js
@@ -0,0 +1,40 @@
+const rules = [
+    {
+        title: '📚 Respectați regulile de bază ale Discordului',
+        description: 'Nu promovați violența, ura, discriminarea, conținutul ilegal sau necorespunzător excesiv.'
+    },
+    {
+        title: '🤝 Fii politicos și respectuos',
+        description: 'Comunică civilizat și respectuos, evitând limbajul ofensator, atacurile personale sau provocările excesive.'
+    },
+    {
+        title: '🚫 Nu spamați',
+        description: 'Evitați mesaje repetitive, publicitate excesivă, majuscule exagerate sau link-uri sau imagini irelevante în exces.'
+    },
+    {
+        title: '🔒 Respectați confidențialitatea',
+        description: 'Nu divulgați informații personale despre alți membri fără consimțământul lor explicit, cum ar fi nume reale, adrese, numere de telefon sau informații financiare.'
+    },
+    {
+        title: '🚷 Nu faceți discriminări sau hărțuire',
+        description: 'Nu tolerați discriminarea, hărțuirea, intimidarea sau comportament inacceptabil bazat pe rasă, sex, religie, orientare sexuală, vârstă, dizabilități sau alte caracteristici personale.'
+    },
+    {
+        title: '📌 Respectați canalele și temele serverului',
+        description: 'Postați în canalele potrivite și respectați temele specifice ale acestora. Evitați off-topicurile sau postarea repetitivă a aceluiași conținut în canale diferite.'
+    },
+    {
+        title: '😠 Nu faceți trolling, flame sau provocări',
+        description: 'Încurajați o comunicare pozitivă și constructivă în comunitate, evitând comportamentul negativ.'
+    },
+    {
+        title: '🌙 Fără @everyone la ore târzii',
+        description: 'Nu scrieți mesaje cu @everyone la ore târzii (de preferat până la ora 22:00).'
+    },
+    {
+        title: '🚫 Reclame interzise',
+        description: 'Reclamele la alte servere de discord/grupuri NU sunt permise.'
+    }
+];
+
+module.exports = rules;
