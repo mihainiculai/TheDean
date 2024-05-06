b5eefbaa79297625aa67c250ee23b86e6d17bd77
Mihai Niculai <nmbroomy@gmail.com>
2024-05-06 15:56:07 +0300

    Refactor /info

---
+++ configs/info.js
@@ -0,0 +1,62 @@
+const info = {
+    title: "📖 Despre server",
+    content: [
+        "**CSIE++** este serverul oficial de Discord al Facultății de Cibernetică, Statistică și Informatică Economică.",
+        "Scopul nostru este de a crea o comunitate unită și de a oferi un loc de discuție și ajutor pentru toți studenții de la CSIE.",
+    ]
+};
+
+const channelsInfo = {
+    title: "📚 Canalele serverului",
+    channels: [
+        {
+            title: "🔴 Anunturi",
+            description: "Oferă anunțuri OFICIALE despre facultate și despre ce se întâmplă pe server."
+        },
+        {
+            title: "👥 Intros",
+            description: "Este un loc bun pentru a te introduce și a face cunoștință cu ceilalți studenți."
+        },
+        {
+            title: "💬 Discuții 1 / 2",
+            description: "Canale pentru discuții generale."
+        },
+        {
+            title: "🆘 Ajutor facultate",
+            description: "Este destinat pentru întrebări și discuții despre materii de facultate."
+        },
+        {
+            title: "📗 CSIE An 1 / 2 / 3",
+            description: "Dacă doriți să comunicați exclusiv în scop academic, avem acest canal."
+        },
+        {
+            title: "🖥 Cariera IT",
+            description: "Este destinat pentru întrebări și discuții despre cariera în IT."
+        },
+        {
+            title: "🛒 Bazar",
+            description: "Este un loc bun pentru a face anunțuri de cumpărare/vânzare, un fel de OLX! 😄"
+        },
+        {
+            title: "💻 Playground",
+            description: "Este destinat folosirii comenzii `/code` care folosește GPT-4 pentru a răspunde la întrebări de programare."
+        },
+        {
+            title: "🆚 Competitiv",
+            description: "Este destinat pentru jocuri amicale și competiții la diferite jocuri."
+        },
+        {
+            title: "🎰 Casino",
+            description: "Este destinat pentru jocuri de noroc: `/slots` `/tictactoe` `/rockpaperscissors`"
+        },
+        {
+            title: "🐸 Memes",
+            description: "Este destinat pentru postarea de meme-uri."
+        }
+    ]
+};
+
+module.exports = {
+    info,
+    channelsInfo
+};
